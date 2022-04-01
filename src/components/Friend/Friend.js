be7193dccb254362d2efda7c49e050633ea1f110
Rumana K <rumana2k20@gmail.com>
2022-04-02 02:37:54 +0600

    used react router and useNavigate

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Friend = (props) => {
+    const { name, username, id } = props.friend
+    
+    let navigate = useNavigate();
+    
+    const showFriendDetails = () =>{
+
+        // const path = `/friends/${username}`
+
+        navigate(`/friends/${username}`);
+    }
+
+    
+    
+    return (
+        <div>
+            <h2>{name} </h2>
+            <button onClick={showFriendDetails} >{username}:{id}</button>
+        </div>
+    );
+};
+
+export default Friend;
